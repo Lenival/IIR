df7ac592d559c23965ce28ac9808a1cb1a0b32f0
LENI <LENI@LAPTOP-0N2LNBLS.home>
2019-10-19 21:32:07 -0300

    Resolvido problema de posição da banda de rejeição do filtro, as equações de projetos são todas na variável omega, estão bastou multiplicar as frequências por 2*pi. Resolvido o problemas de valores complexos que apareciam devida a inprecisão dos coeficientes.

--- filtro_iir.m
+++ filtro_iir.m
@@ -24,8 +24,8 @@ f = (Fs/L)*(-L/2:L/2-1)';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Frequência de corte e passagem normalizadas [rad/s]
-w_s = [1850 2150]./(Fs/2);
-w_p = [1800 2200]./(Fs/2);
+w_s = [1850 2150]./(2*pi*Fs/2);
+w_p = [1800 2200]./(2*pi*Fs/2);
 
 % Cálculo das frequências de corte
 w_c = [(w_p(1) + w_s(1))/2 (w_s(2) + w_p(2))/2];
@@ -38,64 +38,60 @@ delta_w_min = min(delta_w(1),delta_w(2));
 
 % Determinando a mínima ondulação máxima e o Ar 
 delta_min = 0.01;
-Ar = -20*log10(delta_min);
+Arp = -20*log10(1-delta_min);
+Ars = -20*log10(delta_min);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Preparação do ruído
+%% Adicionando sinal ao ruído AWG
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Criando um ruído AWG
-N_0 = 0.01;
-ruido = N_0*randn(L,1);
+N_0 = 0.1;                                 % Densidade espectral do ruído
+ruido = N_0*randn(L,1);                     % Ruído AWG
+banda = [0 w_s(1) w_s(1)+(25/(Fs/2)) w_s(2)-(25/(Fs/2)) w_s(2) 1];% Banda
+b = remez(2500, banda ,[0 0 1 1 0 0]);      % Filtro limitador de ruído
+ruido_limitado = filter(b,1,ruido);         % Limitando o espectro do ruído
+yn = y(:,1)+ruido_limitado;                 % Adicionando sial e ruído
 
-% Filtro limitador de ruído
-b = remez(1500,[0 w_s(1) w_s(1)+(25/(Fs/2)) w_s(2)-(25/(Fs/2)) w_s(2) 1],[0 0 1 1 0 0]);
-% b = remez(1500,[0 1.85e3/(Fs/2) 1.90e3/(Fs/2) 2.1e3/(Fs/2) 2.15e3/(Fs/2) 1],[0 0 1 1 0 0]);
-
-
-% Limitando o espectro do ruído
-ruido_limitado = filter(b,1,ruido);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Projeto do Butterworth
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_b,Wp_b] = buttord(w_p*(Fs/2),w_s*(Fs/2),3,Ar,'s');
-[Z_b,P_b,K_b] = butter(N_b/2,w_c*(Fs/2),'stop','s'); % Para stop retorna 2n
+[N_b,Wp_b] = buttord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[Z_b,P_b,K_b] = butter(N_b,w_c*(Fs/2),'stop','s'); % Para stop retorna 2n
 [B_b,A_b] = zp2tf(Z_b,P_b,K_b);
-[z_b,p_b,k_b] = bilinear(Z_b,P_b,K_b,Fs/2,9400);
-sos_b = zp2sos(z_b,p_b,k_b);
-
+[z_b,p_b,k_b] = bilinear(Z_b,P_b,K_b,Fs);
+[sos_b,g_b] = zp2sos(z_b,p_b,k_b);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Projeto do Chebyshev I  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_c1,Wp_c1] = cheb1ord(w_p*(Fs/2),w_s*(Fs/2),0.0873,Ar,'s');
-[Z_c1,P_c1,K_c1] = cheby1(N_c1,0.0873,w_p*(Fs/2),'stop','s'); % Para stop retorna 2n
+[N_c1,Wp_c1] = cheb1ord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[Z_c1,P_c1,K_c1] = cheby1(N_c1,Arp,w_p*(Fs/2),'stop','s'); % Para stop retorna 2n
 [B_c1,A_c1] = zp2tf(Z_c1,P_c1,K_c1);
-[z_c1,p_c1,k_c1] = bilinear(Z_c1,P_c1,K_c1,Fs,20600);
-sos_c1 = zp2sos(z_c1,p_c1,k_c1);
+[z_c1,p_c1,k_c1] = bilinear(Z_c1,P_c1,K_c1,Fs);
+[sos_c1,g_c1] = zp2sos(z_c1,p_c1,k_c1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Projeto do Chebyshev II
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_c2,Wp_c2] = cheb2ord(w_p*(Fs/2),w_s*(Fs/2),3,Ar,'s');
-[Z_c2,P_c2,K_c2] = cheby2(N_c2,Ar,w_s*(Fs/2),'stop','s'); % Para stop retorna 2n
+[N_c2,Wp_c2] = cheb2ord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[Z_c2,P_c2,K_c2] = cheby2(N_c2,Ars,w_s*(Fs/2),'stop','s'); % Para stop retorna 2n
 [B_c2,A_c2] = zp2tf(Z_c2,P_c2,K_c2);
 [z_c2,p_c2,k_c2] = bilinear(Z_c2,P_c2,K_c2,Fs/2);
-sos_c2 = zp2sos(z_c2,p_c2,k_c2);
+[sos_c2,g_c2] = zp2sos(z_c2,p_c2,k_c2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Projeto do Elíptico
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_e,Wp_e] = ellipord(w_p*(Fs/2),w_s*(Fs/2),3,Ar,'s');
-[Z_e,P_e,K_e] = ellip(N_e,3,Ar,w_p*(Fs/2),'stop','s'); % Para stop retorna 2n
+[N_e,Wp_e] = ellipord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[Z_e,P_e,K_e] = ellip(N_e,Arp,Ars,w_p*(Fs/2),'stop','s'); % Para stop retorna 2n
 [B_e,A_e] = zp2tf(Z_e,P_e,K_e);
 [z_e,p_e,k_e] = bilinear(Z_e,P_e,K_e,Fs/2);
-sos_e = zp2sos(z_e,p_e,k_e);
+[sos_e,g_e] = zp2sos(z_e,p_e,k_e);
 
 
 % [n,Wp] = ellipord(Wp,Ws,Rp,Rs,'s')
@@ -107,20 +103,18 @@ sos_e = zp2sos(z_e,p_e,k_e);
 %wvtool(w)
 %stem(h_n)
 %freqz(h_n)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Adição do sinal e ruído
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-yn = y(:,1)+ruido_limitado;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Filtragem do sinal
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-y_b = sosfilt(sos_b,yn);% Projeto do Butterworth
-y_c1 = sosfilt(sos_c1,yn);% Projeto do Chebyshev I
-y_c2 = sosfilt(sos_c2,yn);% Projeto do Chebyshev II
-y_e = sosfilt(sos_e,yn);% Projeto do Elíptico
+y_b = sosfilt(sos_b*real(g_b),yn);% Projeto do Butterworth
+y_c1 = sosfilt(sos_c1*real(g_c1),yn);% Projeto do Chebyshev I
+y_c2 = sosfilt(sos_c2*real(g_c2),yn);% Projeto do Chebyshev II
+y_e = sosfilt(sos_e*real(g_e),yn);% Projeto do Elíptico
 
+syms s z
+f_zk = @(z,sk,Ts) (((z*Ts/2)+1)/((z*Ts/2)-1))-sk;
+f_sk = @(s,zk,Ts) (2/Ts)*((s-1)/(s+1))-zk;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -173,7 +167,7 @@ subplot(2,1,2)
 
  
 Y_c1 = fftshift(fft(y_c1));
-plot(f,abs(Y_c1),'m');title('Sinal após o filtro elíptico');xlim([0 Fs/2]);xlabel('f (Hz)');ylabel('Y_{e}(\omega)')
+plot(f,abs(Y_c1),'m');title('Sinal após o filtro Chebyshev I');xlim([0 Fs/2]);xlabel('f (Hz)');ylabel('Y_{cI}(\omega)')
 
 
 % Espectro da resposta desejada
