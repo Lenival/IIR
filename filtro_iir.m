3aa1cb8cb03ade19e91e6215f13c9ec947950b3e
lenival <lenival@ufrn.edu.br>
2019-10-26 00:08:43 -0300

    Inicializando as frequências com valores em radianos. Removidos alguns parâmetros e constantes não utilizados

--- filtro_iir.m
+++ filtro_iir.m
@@ -1,5 +1,7 @@
 %% Projeto de Filtros IIR para disciplina Processamento Digital de Sinais
 %  Aluno: José Lenival Gomes de França
+%%
+% Este s
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Preparação do sinal
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -24,8 +26,8 @@ f = (Fs/L)*(-L/2:L/2-1)';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Frequência de corte e passagem normalizadas [rad/s]
-w_s = 2*pi*[1850 2150]./(Fs/2);
-w_p = 2*pi*[1800 2200]./(Fs/2);
+w_s = 2*pi*[1850 2150];
+w_p = 2*pi*[1800 2200];
 % Cálculo das frequências de corte
 w_c = [(w_p(1) + w_s(1))/2 (w_s(2) + w_p(2))/2];
 % Determinando a mínima ondulação máxima e o Ar 
@@ -39,7 +41,7 @@ Ars = -20*log10(delta_min);
 
 N_0 = 0.001;                                 % Densidade espectral do ruído
 ruido = N_0.*randn(L,1);                     % Ruído AWG
-[z_n,p_n,k_n] = ellip(50,Arp,Ars,w_s/(2*pi),'bandpass');
+[z_n,p_n,k_n] = ellip(50,Arp,Ars,w_s/(Fs*pi),'bandpass');
 [sos_n,g_n] = zp2sos(z_n,p_n,k_n);
 ruido_limitado = sosfilt(sos_n*real(g_n),ruido);
 yn = y(:,1)+ruido_limitado;                 % Adicionando sial e ruído
@@ -49,9 +51,9 @@ yn = y(:,1)+ruido_limitado;                 % Adicionando sial e ruído
 %% Projeto do Butterworth
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_b,Wp_b] = buttord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[N_b,Wp_b] = buttord(w_p,w_s,Arp,Ars,'s');
 [Z_b,P_b,K_b] = butter(N_b,Wp_b,'stop','s'); % Para stop retorna 2n
-[B_b,A_b] = zp2tf(Z_b,P_b,K_b);
+[B_b,A_b] = zp2tf(Z_b,P_b,K_b);  %Gera erros numéricos com N alto
 [z_b,p_b,k_b] = bilinear(Z_b,P_b,K_b,Fs,2000);
 [sos_b,g_b] = zp2sos(z_b,p_b,k_b);
 
@@ -59,9 +61,9 @@ yn = y(:,1)+ruido_limitado;                 % Adicionando sial e ruído
 %% Projeto do Chebyshev I  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_c1,Wp_c1] = cheb1ord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[N_c1,Wp_c1] = cheb1ord(w_p,w_s,Arp,Ars,'s');
 [Z_c1,P_c1,K_c1] = cheby1(N_c1,Arp,Wp_c1,'stop','s'); % Para stop retorna 2n
-[B_c1,A_c1] = zp2tf(Z_c1,P_c1,K_c1);
+% [B_c1,A_c1] = zp2tf(Z_c1,P_c1,K_c1);    Gera erros numéricos com N alto
 [z_c1,p_c1,k_c1] = bilinear(Z_c1,P_c1,K_c1,Fs,2000);
 [sos_c1,g_c1] = zp2sos(z_c1,p_c1,k_c1);
 
@@ -69,9 +71,9 @@ yn = y(:,1)+ruido_limitado;                 % Adicionando sial e ruído
 %% Projeto do Chebyshev II
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_c2,Wp_c2] = cheb2ord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[N_c2,Wp_c2] = cheb2ord(w_p,w_s,Arp,Ars,'s');
 [Z_c2,P_c2,K_c2] = cheby2(N_c2,Ars,Wp_c2,'stop','s'); % Para stop retorna 2n
-[B_c2,A_c2] = zp2tf(Z_c2,P_c2,K_c2);
+% [B_c2,A_c2] = zp2tf(Z_c2,P_c2,K_c2);  Gera erros numéricos com N alto
 [z_c2,p_c2,k_c2] = bilinear(Z_c2,P_c2,K_c2,Fs,2000);
 [sos_c2,g_c2] = zp2sos(z_c2,p_c2,k_c2);
 
@@ -79,16 +81,28 @@ yn = y(:,1)+ruido_limitado;                 % Adicionando sial e ruído
 %% Projeto do Elíptico
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[N_e,Wp_e] = ellipord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s');
+[N_e,Wp_e] = ellipord(w_p,w_s,Arp,Ars,'s');
 [Z_e,P_e,K_e] = ellip(N_e,Arp,Ars,Wp_e,'stop','s'); % Para stop retorna 2n
-[B_e,A_e] = zp2tf(Z_e,P_e,K_e);
+% [B_e,A_e] = zp2tf(Z_e,P_e,K_e);    Gera erros numéricos com N alto
 [z_e,p_e,k_e] = bilinear(Z_e,P_e,K_e,Fs,2000);
 [sos_e,g_e] = zp2sos(z_e,p_e,k_e);
 
-% fvtool(sos_b,sos_c1,sos_c2,sos_e)
+fvtool(sos_b,sos_c1,sos_c2,sos_e);
+
+
 %wvtool(w)
 %stem(h_n)
 %freqz(h_n)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Projeto do Butterworth errado
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% [N_b,Wp_b] = buttord(w_p*(Fs/2),w_s*(Fs/2),Arp,Ars,'s')
+% [B_b,A_b] = butter(N_b,Wp_b,'stop','s'); % Para stop retorna 2n
+[b_b,a_b] = bilinear(B_b,A_b,Fs,2000);
+y_berro = filter(b_b,a_b,yn);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Filtragem do sinal
